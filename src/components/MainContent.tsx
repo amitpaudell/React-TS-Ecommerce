e82d63b7b44a6bf7bf63404dcc541afed97a416e
Amit Paudel <amitpaudel789@gmail.com>
2026-01-23 22:38:28 +0545

    pagination

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -78,7 +78,34 @@ const MainContent = () => {
   };
 
   const filteredProduct = getFilteredProduct();
-  console.log(filteredProduct);
+
+  const totalProducts = 100;
+  const totalPages = Math.ceil(totalProducts / itemsPerPage);
+
+  const handlePageChange = (page: number) => {
+    if (page > 0 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
+  const getPaginationButtons = () => {
+    const buttons: number[] = [];
+    let startPage = Math.max(1, currentPage - 2);
+    let endPage = Math.min(totalPages, currentPage + 2);
+
+    if (currentPage - 2 < 1) {
+      endPage = Math.min(totalPages, endPage + (2 - currentPage - 1));
+    }
+
+    if (currentPage + 2 > totalPages) {
+      startPage = Math.min(1, startPage - (2 - totalPages - currentPage));
+    }
+
+    for (let page = startPage; page <= endPage; page++) {
+      buttons.push(page);
+    }
+    return buttons;
+  };
+
   return (
     <div className="rounded w-full flex justify-between flex-wrap">
       <section className="xl:w-[55rem] lg:w-[55rem] sm:w-[40rem] xs:w-[20rem] p-5">
@@ -132,6 +159,41 @@ const MainContent = () => {
                 price={product.price}
               ></BookCard>
             ))}
+          </div>
+
+          <div className="flex flex-col sm:flex-row justify-between items-center mt-5">
+            <button
+              onClick={() => handlePageChange(currentPage - 1)}
+              disabled={currentPage == 1}
+              className="border px-4 py-2 mx-2 rounded-full"
+            >
+              Previous
+            </button>
+
+            {/* 
+              1,2,3,4,5 */}
+
+            <div className="flex flex-wrap justify-center">
+              {/* pagination */}
+              {getPaginationButtons().map((page) => (
+                <button
+                  key={page}
+                  onClick={() => handlePageChange(page)}
+                  className={`border px-4 py-2 mx-1 rounded-full ${
+                    page === currentPage ? 'bg-black text-white' : ''
+                  }`}
+                >
+                  {page}
+                </button>
+              ))}
+            </div>
+            <button
+              onClick={() => handlePageChange(currentPage + 1)}
+              disabled={currentPage === totalPages}
+              className="border px-4 py-2 mx-2 rounded-full"
+            >
+              Next
+            </button>
           </div>
         </div>
       </section>
